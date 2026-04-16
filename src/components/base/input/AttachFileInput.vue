d0b07618d950dbb6eab3c98a4120a75ae7ea542d
nopparat <nopparat.cysit@gmail.com>
2026-04-16 09:10:04 +0700

    refactor: Remove unused utility import in AttachFileInput component
    - Eliminated the import of fileNameFromAttachmentUrl from AttachFileInput.vue as it was not being utilized, streamlining the component's code.

--- src/components/base/input/AttachFileInput.vue
+++ src/components/base/input/AttachFileInput.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from "vue";
-import { cn, fileNameFromAttachmentUrl } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import { FileText, Plus, X } from "lucide-vue-next";
 import { ref } from "vue";
 
